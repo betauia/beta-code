1afa454172eb8a06ea22838c841e6f649d8abb72
Ingeborg <ingeborg.t.briskodden@gmail.com>
2026-01-31 23:00:51 +0100

    frontend

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,4 +2,20 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      fs: {
+        allow: [
+          // Allow serving files from the project root
+          '.',
+          // Allow serving files from node_modules
+          '/mnt/c/Users/ingeb/projects/beta-code/beta-code/node_modules',
+        ]
+      }, // <-- Added comma here
+      watch: {
+        usePolling: true,
+      }
+    }
+  }
+})
