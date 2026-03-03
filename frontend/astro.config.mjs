91d45353e04b0230003d8619c594c696c4cfe849
Herator <herman.ardem@gmail.com>
2026-03-03 01:02:32 +0100

    Added small improvments

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    server: {
-       host: true,
-       port:4321
-    }
+  output: 'server',
+
+  server: {
+     host: true,
+     port:4321
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
